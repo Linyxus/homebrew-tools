b949871abaa1719432f4f2177068c3dfbe565b77
Yichen Xu <yichen.x@outlook.com>
2020-04-10 21:52:28 +0800

    Fix description of BaiduPCS-go

--- baidupcs-go.rb
+++ baidupcs-go.rb
@@ -1,5 +1,5 @@
 class BaidupcsGo < Formula
-    desc "A simple tool for cloning Codeforces contests onto Virtual Judge, in one line"
+    desc "A simple CLI tool for accessing Baidu Cloud"
     homepage "https://github.com/iikira/BaiduPCS-Go"
     url "https://github.com/iikira/BaiduPCS-Go/releases/download/v3.6.2/BaiduPCS-Go-v3.6.2-darwin-osx-amd64.zip"
     sha256 "547ab39ab4f205c4323337a8e0c250ab6f51502a3812fb6bd5a32bf2b25b0f14"
